35972f87f441360269de3f3778499cc15ec0201e
Caxivitual <destinyhero789@gmail.com>
2025-04-04 22:00:29 +0700

    ResourceHandle: swap function naming between Convert and ConvertUnchecked

--- Lunacub/ResourceHandle.cs
+++ Lunacub/ResourceHandle.cs
@@ -3,21 +3,21 @@
 namespace Caxivitual.Lunacub;
 
 public readonly record struct ResourceHandle(ResourceID Rid, object? Value) {
-    public ResourceHandle<T> Convert<T>() where T : class {
+    public ResourceHandle<T> ConvertUnchecked<T>() where T : class {
         return new(Rid, (T)Value!);
     }
     
-    public ResourceHandle<T> ConvertUnchecked<T>() where T : class {
+    public ResourceHandle<T> Convert<T>() where T : class {
         return new(Rid, Value as T);
     }
 }
 
 public readonly record struct ResourceHandle<T>(ResourceID Rid, T? Value) where T : class {
-    public ResourceHandle<TOther> Convert<TOther>() where TOther : class {
+    public ResourceHandle<TOther> ConvertUnchecked<TOther>() where TOther : class {
         return new(Rid, (TOther)(object)Value!);
     }
     
-    public ResourceHandle<TOther> ConvertUnchecked<TOther>() where TOther : class {
+    public ResourceHandle<TOther> Convert<TOther>() where TOther : class {
         return new(Rid, Value as TOther);
     }
     
