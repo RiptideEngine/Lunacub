ee38c015df3f63f018f4f6467ba1078a66e3d2bf
Caxivitual <destinyhero789@gmail.com>
2025-06-22 06:19:05 +0700

    Importing: remove fields in ResourceContainer

--- sources/Lunacub.Importing/ResourceContainer.cs
+++ sources/Lunacub.Importing/ResourceContainer.cs
@@ -5,14 +5,6 @@ namespace Caxivitual.Lunacub.Importing;
 internal sealed class ResourceContainer {
     public readonly ResourceID ResourceId;
 
-    public ushort MajorVersion;
-    public ushort MinorVersion;
-    
-    public uint ResourceBlobPosition;
-    public uint OptionBlobPosition;
-    public ImmutableArray<string> Tags;
-    public string DeserializerName;
-
     public Task<DeserializeResult>? VesselImportTask;
     public Task<object?>? FullImportTask;
     public uint ReferenceCount;
@@ -22,15 +14,5 @@ internal sealed class ResourceContainer {
         ResourceId = resourceId;
         ReferenceCount = 1;
         CancellationTokenSource = new();
-        Tags = ImmutableArray<string>.Empty;
-        DeserializerName = string.Empty;
     }
-    
-    // public ResourceContainer(ResourceID resourceId, uint initialReferenceCount) {
-    //     ResourceId = resourceId;
-    //     ReferenceCount = initialReferenceCount;
-    //     FullImportTask = null!;
-    //     VesselImportTask = null!;
-    //     CancellationTokenSource = new();
-    // }
 }
