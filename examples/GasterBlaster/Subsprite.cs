12b90e8621632c18079e81275b3d05277150432f
Caxivitual <destinyhero789@gmail.com>
2025-07-29 15:01:40 +0700

    remove gaster blaster example

--- examples/GasterBlaster/Subsprite.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Caxivitual.Lunacub.Examples.GasterBlaster;
-
-public readonly record struct Subsprite(string Name, Rectangle<uint> Region);
--- examples/GasterBlaster/Vertex.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Numerics;
-
-namespace Caxivitual.Lunacub.Examples.GasterBlaster;
-
-public readonly record struct Vertex(Vector3 Position, Vector2 TexCoord0);
